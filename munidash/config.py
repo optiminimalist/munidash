1fed0d301c08c0cfd2841fef709299cb7d4ba4d1
Michael Luckeneder <michael@luckeneder.net>
2018-07-29 10:26:30 -0700

    added twitter loader

--- munidash/config.py
+++ munidash/config.py
@@ -2,5 +2,9 @@ import os
 import urllib
 
 REDIS_URL = urllib.parse.urlparse(os.environ.get('REDISCLOUD_URL', "redis://localhost:6379/"))
+TWITTER_API_KEY = os.environ.get('TWITTER_API_KEY')
+TWITTER_SECRET_KEY = os.environ.get('TWITTER_SECRET_KEY')
+TWITTER_ACCESS_TOKEN = os.environ.get('TWITTER_ACCESS_TOKEN')
+TWITTER_ACCESS_TOKEN_SECRET = os.environ.get('TWITTER_ACCESS_TOKEN_SECRET')
 
 MUNI_METRO_ROUTES = frozenset(["N", "M", "L", "KT", "J", "S", "MBUS"])
